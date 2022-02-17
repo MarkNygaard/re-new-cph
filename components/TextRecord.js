10b6d93c452143b805b824597a7bd2d63b2dac8a
Mark Nygaard <marknygaard@hotmail.com>
2022-02-17 23:31:52 +0100

    tailwind typography added

--- components/TextRecord.js
+++ components/TextRecord.js
@@ -3,8 +3,10 @@ import { StructuredText } from 'react-datocms';
 
 export default function TextRecord({ details }) {
   return (
-    <div id={details.navigationId} className="bg-gray-500 py-20 px-10">
-      <StructuredText data={details.content} />
+    <div id={details.navigationId} className="py-20 px-10 flex justify-center">
+      <div className="prose prose-gray w-screen">
+        <StructuredText data={details.content} />
+      </div>
     </div>
   );
 }
