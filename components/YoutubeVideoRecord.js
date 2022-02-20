289b0f1d547c93230a3513bf98844d8d05063537
Mark Nygaard <marknygaard@hotmail.com>
2022-02-20 22:43:19 +0100

    Youtube lazy load added

--- components/YoutubeVideoRecord.js
+++ components/YoutubeVideoRecord.js
@@ -1,18 +1,12 @@
 import React from 'react';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 
 export default function YoutubeVideoRecord({ details }) {
   return (
     <div className="flex justify-center bg-gray-200 py-10">
       <div className="mx-auto w-full max-w-4xl p-8">
         <div className="shadow-x1 rounded-lg bg-white p-2 sm:p-4">
-          <iframe
-            className="aspect-video w-full"
-            src={'https://www.youtube.com/embed/' + details.link.providerUid}
-            title="Youtube video player"
-            frameBorder={0}
-            allow="accelerometer; autoplay; clipboard; encrypted-media; gyroscope; picture in picture"
-            allowFullScreen
-          ></iframe>
+          <LiteYouTubeEmbed id={details.link.providerUid} title="Play" />
         </div>
       </div>
     </div>
