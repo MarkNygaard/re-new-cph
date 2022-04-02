8de382b846975876c443a8e8cc0b8ea5b0ca6f90
Mark Nygaard <mark.nygaard@hotmail.com>
2022-04-02 16:01:21 +0200

    language added to HTML and contrast fixed

--- components/PricingCard.js
+++ components/PricingCard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function PricingCard({ details }) {
   return (
     <div className="w-full max-w-sm rounded-lg bg-gray-500 p-6">
-      <h3 className="mb-4 text-xl uppercase text-white">{details.title}</h3>
-      <p>
+      <div className="mb-4 text-xl font-semibold uppercase text-white">
+        {details.title}
+      </div>
+      <p className="text-gray-100">
         <span>{details.price}</span>
       </p>
       <ul>
-        {details.description.split("*").map((bullet, index) => (
-          <li key={index} className="my-2 text-sm text-gray-700">
+        {details.description.split('*').map((bullet, index) => (
+          <li key={index} className="my-2 text-sm text-gray-100">
             {bullet}
           </li>
         ))}
