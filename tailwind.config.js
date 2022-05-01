6f34e8159d7b37676395f4a73b71bb98d617b73a
Mark Nygaard <mark.nygaard@hotmail.com>
2022-05-01 17:46:02 +0200

    safelist changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       },
     },
   },
-  safelist: ['grid-cols-2', 'grid-cols-3', 'grid-cols-4', 'gap-2', 'gap-4'],
+  safelist: [
+    'lg:grid-cols-1',
+    'grid-cols-2',
+    'lg:grid-cols-2',
+    'grid-cols-3',
+    'lg:grid-cols-3',
+    'grid-cols-4',
+    'lg:grid-cols-4',
+    'gap-2',
+    'gap-4',
+  ],
   plugins: [require('@tailwindcss/typography')],
 };
