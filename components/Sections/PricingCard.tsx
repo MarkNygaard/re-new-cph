8a97f10788bf109e664a3d5f976ac8ff7d86edcd
Mark Nygaard <mark.nygaard@hotmail.com>
2022-05-14 01:49:15 +0200

    Changed contrast on PriceCards

--- components/Sections/PricingCard.tsx
+++ components/Sections/PricingCard.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default function PricingCard({ details }) {
   return (
-    <div className="w-full rounded-lg bg-gray-500 p-6">
+    <div className="w-full rounded-lg bg-gray-600 p-6">
       <div className="mb-4 text-xl font-semibold uppercase text-white">
         {details.title}
       </div>
-      <p className="text-gray-100">
+      <p className="text-white">
         <span>{details.price}</span>
       </p>
       <ul>
         {details.description.split('*').map((bullet, index) => (
-          <li key={index} className="my-2 text-sm text-gray-100">
+          <li key={index} className="my-2 text-sm text-white">
             {bullet}
           </li>
         ))}
