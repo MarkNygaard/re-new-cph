f7c5b2998b410986c3feb2bab1f977bbb59acfe3
Mark Nygaard <mark.nygaard@hotmail.com>
2022-10-19 17:19:09 +0200

    updated all packages

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -1,13 +1,28 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
-    // Regenerate our index route showing the images
-    await res.unstable_revalidate('/');
-    return res.json({ revalidated: true });
+    // Check that body is not empty
+    const body = req.body;
+    if (!body) {
+      res.status(400).send('Bad request (no body)');
+      return;
+    }
+
+    // Get the slug to revalidate from body
+    const slugToRevalidate = body.slugToRevalidate;
+    if (slugToRevalidate) {
+      await res.revalidate('/');
+      return res.json({ revalidated: true });
+    }
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
